a57950c64f0bf4b47a1c4cf2448eee5b4561d071
Markus Iorio <mbalzert@gmail.com>
2021-03-29 08:54:21 +0200

    Aufgabe 3 implementiert

--- oeffnen_db.py
+++ oeffnen_db.py
@@ -1,16 +1,24 @@
-import sqlite3 #import der lib
+import sqlite3, csv, argparse #import der lib
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--table', action='store',
+                    dest='table_name',
+                    help='Store a table')
+table = parser.parse_args()
 
 conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
 
 cur = conn.cursor()
 # Ansprechen der lokalen sqlite Datenbank
-cur.execute("SELECT name \
+cur.execute(f"SELECT * \
                     FROM \
-                    sqlite_master \
-                    WHERE \
-                    type ='table' AND \
-                    name NOT LIKE 'sqlite_%';")
+                    {table.table_name} \
+            ;")
 
+data_csv = "data.csv"
 
-print(cur.fetchall())
+list_data = cur.fetchall()
+with open(data_csv, 'w', encoding="utf-8") as f:
+    writer = csv.writer(f, delimiter=",", lineterminator="\n", quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerows(list_data)
 conn.close() 
