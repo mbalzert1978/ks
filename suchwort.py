1f2e3d4af2032311a6df7bc6a7062a316ee092cc
Markus Iorio <mbalzert@gmail.com>
2021-04-02 21:07:21 +0200

    finale

--- suchwort.py
+++ suchwort.py
@@ -1,12 +1,11 @@
-import sqlite3, pprint, argparse #import der lib
+import sqlite3, argparse #import der lib
 from openpyxl import Workbook #import der lib
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--search', action='store',
                     dest='search_',
-                    help='Store a table')
+                    help='Store a table', default="Rock")
 searching_ = parser.parse_args()
-
 
 conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
 cur = conn.cursor()
@@ -21,52 +20,62 @@ cur.execute(f"select name as Songtitel,                                         
                 (select Album.Title from Album                                          \
                     WHERE Track.GenreId = Album.AlbumId) AS Albumname                   \
                 from Track                                                              \
-                where name like '%Rock%'                                \
+                where name like '%{searching_.search_}%'                                \
                 order by AlbumId                                                        \
                         ;")
-#{searching_.search_}
+
 
 list_data = cur.fetchall()
-hilfs_var = 1
-counter = 1
+dict_ = {}
 for item in list_data:
     a, b, c, d, e = item
-    if hilfs_var == 1:
-        wb = Workbook()
-    if len(e) > 15:
-        e = e[0:14]
-        ws = wb.active
-        ws.title = f"{e}"
-    if hilfs_var == 1:
-        hilfs_var = e
-        _ = ws.cell(column=1, row=counter, value="Songtitle")
-        _ = ws.cell(column=2, row=counter, value="Künstler")
-        _ = ws.cell(column=3, row=counter, value="MediaTyp")
-        _ = ws.cell(column=4, row=counter, value="Genre")
-        counter += 1
-        _ = ws.cell(column=1, row=counter, value=a)
-        _ = ws.cell(column=2, row=counter, value=b)
-        _ = ws.cell(column=3, row=counter, value=c)
-        _ = ws.cell(column=4, row=counter, value=d)
-    elif hilfs_var == e:
-        counter += 1
-        _ = ws.cell(column=1, row=counter, value=a)
-        _ = ws.cell(column=2, row=counter, value=b)
-        _ = ws.cell(column=3, row=counter, value=c)
-        _ = ws.cell(column=4, row=counter, value=d)
+    if e in dict_:
+        dict_[e] += (a, b, c, d)
     else:
-        counter = 1
-        wb.create_sheet(f"{e}")
-        _ = ws.cell(column=1, row=counter, value="Songtitle")
-        _ = ws.cell(column=2, row=counter, value="Künstler")
-        _ = ws.cell(column=3, row=counter, value="MediaTyp")
-        _ = ws.cell(column=4, row=counter, value="Genre")
-        counter += 1
-        _ = ws.cell(column=1, row=counter, value=a)
-        _ = ws.cell(column=2, row=counter, value=b)
-        _ = ws.cell(column=3, row=counter, value=c)
-        _ = ws.cell(column=4, row=counter, value=d)
-        counter = 1
-        hilfs_var = e
-wb.save(f"Rock.xlsx")
+        dict_.update({e : (a,b,c,d)})
+        
+wb = Workbook()
+ws = wb.worksheets[0]
+ccount = 1
+rcount = 2
+sheetcount = 1
+for key, value in dict_.items():
+    if len(key) > 15:
+        key = key[0:14] + "..."
+    if ws.title == "Sheet":
+        ws.title = key
+        _ = ws.cell(column=1, row=1, value="Songtitel")
+        _ = ws.cell(column=2, row=1, value="Künstler")
+        _ = ws.cell(column=3, row=1, value="Mediatyp")
+        _ = ws.cell(column=4, row=1, value="Genre")
+        for item in value:
+            if ccount  <=4:
+                _ = ws.cell(column=ccount, row=rcount, value=item)
+                ccount +=1
+            else:
+                ccount = 1
+                _ = ws.cell(column=ccount, row=rcount+1, value=item)
+                ccount = 2
+                rcount +=1
+        rcount = 1
+    else:
+        wb.create_sheet(key)
+        ws = wb.worksheets[sheetcount]
+        _ = ws.cell(column=1, row=1, value="Songtitel")
+        _ = ws.cell(column=2, row=1, value="Künstler")
+        _ = ws.cell(column=3, row=1, value="Mediatyp")
+        _ = ws.cell(column=4, row=1, value="Genre")
+        for item in value:
+            if ccount  <=4:
+                _ = ws.cell(column=ccount, row=rcount, value=item)
+                ccount +=1
+            else:
+                ccount = 1
+                _ = ws.cell(column=ccount, row=rcount+1, value=item)
+                ccount = 2
+                rcount +=1 
+        sheetcount += 1
+        rcount = 1
+wb.save(f"{searching_.search_}.xlsx")
+
 conn.close() 
