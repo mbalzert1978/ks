f339aa0f52bf0aff439cd5e731fd250fed4f9a50
Markus Iorio <mbalzert@gmail.com>
2021-04-02 22:19:50 +0200

    in funktion gewandelt

--- oeffnen_db.py
+++ oeffnen_db.py
@@ -1,18 +1,17 @@
-import sqlite3, csv, io #import der lib
+import sqlite3, csv #import der lib
+def export_customer_to_csv():
+    conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
 
-conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
-
-cur = conn.cursor()
-# Ansprechen der lokalen sqlite Datenbank
-cur.execute("SELECT * \
-                    FROM \
-                    Customer \
-            ;")
-
-data_csv = "data.csv"
-
-list_data = cur.fetchall()
-with open(data_csv, 'w', encoding="utf-8") as f:
-    writer = csv.writer(f, delimiter=";", lineterminator="\n", quoting=csv.QUOTE_NONNUMERIC)
-    writer.writerows(list_data)
-conn.close() 
+    cur = conn.cursor()
+    # Ansprechen der lokalen sqlite Datenbank
+    cur.execute("SELECT * \
+                        FROM \
+                        Customer \
+                ;")
+    data_csv = "data.csv"
+    list_data = cur.fetchall()
+    with open(data_csv, 'w', encoding="utf-8") as f:
+        writer = csv.writer(f, delimiter=";", lineterminator="\n", quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(list_data)
+    conn.close() 
+export_customer_to_csv()
