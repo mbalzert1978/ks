7b3269202d2a06471173bfe20038abd6f05203bd
Markus Iorio <mbalzert@gmail.com>
2021-04-02 22:12:35 +0200

    Update oeffnen_db.py
    funktion erstellt

--- oeffnen_db.py
+++ oeffnen_db.py
@@ -1,16 +1,17 @@
 import sqlite3 #import der lib
 
-conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
 
-cur = conn.cursor()
-# Ansprechen der lokalen sqlite Datenbank
-cur.execute("SELECT name \
-                    FROM \
-                    sqlite_master \
-                    WHERE \
-                    type ='table' AND \
-                    name NOT LIKE 'sqlite_%';")
+def show_tables():
+    conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
 
-
-print(cur.fetchall())
-conn.close() 
+    cur = conn.cursor()
+    # Ansprechen der lokalen sqlite Datenbank
+    cur.execute("SELECT name \
+                        FROM \
+                        sqlite_master \
+                        WHERE \
+                        type ='table' AND \
+                        name NOT LIKE 'sqlite_%';")
+    return cur.fetchall()
+    conn.close() 
+print(show_tables())
