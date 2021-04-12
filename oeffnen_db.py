024a789ae30b909b6b60673f3c1a0f5ce9834ac5
Markus Iorio <mbalzert@gmail.com>
2021-04-12 22:45:19 +0200

    Update oeffnen_db.py
    doppelte klammern entfernt

--- oeffnen_db.py
+++ oeffnen_db.py
@@ -9,7 +9,7 @@ def export_customer_to_csv():
                         Customer \
                 ;")
     list_data = cur.fetchall()
-    with open((os.path.join(os.path.dirname(__file__), "data.csv")), 'w', encoding="utf-8") as f:
+    with open(os.path.join(os.path.dirname(__file__), "data.csv"), 'w', encoding="utf-8") as f:
         writer = csv.writer(f, delimiter=";", lineterminator="\n", quoting=csv.QUOTE_NONNUMERIC)
         writer.writerows(list_data)
     conn.close() 
