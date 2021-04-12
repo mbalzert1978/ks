67e72f087c0d8000d3df055c4f52f8a2535203e4
Markus Iorio <mbalzert@gmail.com>
2021-04-12 22:54:42 +0200

    Update genre_desc.py
    entfernen ungenutzt argumentparser;
    filename dynamischer

--- genre_desc.py
+++ genre_desc.py
@@ -1,22 +1,13 @@
-import sqlite3, argparse, pprint #import der lib
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--table', action='store',
-                    dest='table_name',
-                    help='Store a table')
-table = parser.parse_args()
-
-conn = sqlite3.connect("Chinook_Sqlite_AutoIncrementPKs.sqlite")
+import sqlite3, pprint, os #import der lib
+conn = sqlite3.connect(os.path.join(os.path.dirname(__file__),"Chinook_Sqlite_AutoIncrementPKs.sqlite"))
 
 cur = conn.cursor()
 # Ansprechen der lokalen sqlite Datenbank
-cur.execute(f"SELECT g.name,\
-		        (SELECT count(*) FROM Track AS t \
+cur.execute(f"SELECT g.name,                                    \
+		        (SELECT count(*) FROM Track AS t                \
 			        WHERE g.GenreId = t.GenreId) AS Count_Genre \
-            from Genre AS g \
+            from Genre AS g                                     \
             order by Count_Genre DESC;")
-
-data_csv = "data.csv"
 
 list_data = cur.fetchall()
 pprint.pprint(list_data)
