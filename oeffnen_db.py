3f4309f90257095b3647355dc77ed6296f0a5f31
Markus Iorio <m.iorio@constractiv.de>
2022-01-18 12:06:27 +0100

    refactor

--- oeffnen_db.py
+++ oeffnen_db.py
@@ -1,16 +1,31 @@
-import sqlite3, csv, os #import der lib
+import sqlite3
+import csv
+import os
+
+
 def export_customer_to_csv():
-    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), "Chinook_Sqlite_AutoIncrementPKs.sqlite" ))
+    conn = sqlite3.connect(
+        os.path.join(
+            os.path.dirname(__file__), "Chinook_Sqlite_AutoIncrementPKs.sqlite"
+        )
+    )
 
     cur = conn.cursor()
-    # Ansprechen der lokalen sqlite Datenbank
-    cur.execute("SELECT * \
-                        FROM \
-                        Customer \
-                ;")
+    querry = """
+    SELECT * FROM Customer;
+    """
+    cur.execute(querry)
     list_data = cur.fetchall()
-    with open(os.path.join(os.path.dirname(__file__), "data.csv"), 'w', encoding="utf-8") as f:
-        writer = csv.writer(f, delimiter=";", lineterminator="\n", quoting=csv.QUOTE_NONNUMERIC)
+    with open(
+        os.path.join(os.path.dirname(__file__), "data.csv"),
+        "w",
+        encoding="utf-8",
+    ) as f:
+        writer = csv.writer(
+            f, delimiter=";", lineterminator="\n", quoting=csv.QUOTE_NONNUMERIC
+        )
         writer.writerows(list_data)
-    conn.close() 
+    conn.close()
+
+
 export_customer_to_csv()
