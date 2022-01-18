8be7046153b58758c4e07c17a4070cb96c2d03ea
Markus Iorio <m.iorio@constractiv.de>
2022-01-18 12:03:49 +0100

    refactor

--- oeffnen_db.py
+++ oeffnen_db.py
@@ -1,18 +1,22 @@
-import sqlite3, os #import der lib
+import sqlite3, os  # import der lib
 
 
 def show_tables():
-    conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), "Chinook_Sqlite_AutoIncrementPKs.sqlite" ))
-
+    conn = sqlite3.connect(
+        os.path.join(
+            os.path.dirname(__file__), "Chinook_Sqlite_AutoIncrementPKs.sqlite"
+        )
+    )
     cur = conn.cursor()
     # Ansprechen der lokalen sqlite Datenbank
-    cur.execute("SELECT name \
-                        FROM \
-                        sqlite_master \
-                        WHERE \
-                        type ='table' AND \
-                        name NOT LIKE 'sqlite_%';")
+    querry = """
+    SELECT name FROM sqlite_master\
+         WHERE type ='table' AND name NOT LIKE 'sqlite_%';
+    """
+    cur.execute(querry)
     return_value = cur.fetchall()
     conn.close()
     return return_value
+
+
 print(show_tables())
