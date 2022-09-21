d7975cdfea594443a14587d3000a1ddc1db8f24c
Markus Iorio <markus.iorio@centralnic.com>
2022-09-21 13:44:46 +0000

    model and formating

--- create_model.py
+++ create_model.py
@@ -1,9 +1,13 @@
 import argparse
+import io
 import sys
-from pwiz import print_models, make_introspector
+from contextlib import redirect_stdout
+
+import black
+from pwiz import make_introspector, print_models
 
 
-def create_parser(args) -> argparse.ArgumentParser:
+def create_parser(args) -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         "Script to create an model to connect to an SQLite database."
     )
@@ -25,8 +29,43 @@ def create_parser(args) -> argparse.ArgumentParser:
     return parser.parse_args(args)
 
 
+def create_model(args):
+    model = get_model_str(args)
+    model = fix_unknown_fields(model)
+    model = format_str(model)
+    return model
+
+
+def format_str(model: str):
+    return black.format_str(
+        model,
+        mode=black.Mode(
+            target_versions={black.TargetVersion.PY36},
+            line_length=79,
+            string_normalization=False,
+            is_pyi=False,
+        ),
+    )
+
+
+def get_model_str(args):
+    intro_spector = make_introspector("sqlite", args.database)
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        print_models(intro_spector)
+    return buffer.getvalue()
+
+
+def fix_unknown_fields(model: str):
+    return model[:158] + model[159:].replace(r"UnknownField", r"TextField")
+
+
+def save_model(args, model: str):
+    with open(args.modelname, "w") as file:
+        file.writelines(model)
+
+
 if __name__ == "__main__":
     args = create_parser(sys.argv[1:])
-    intro = make_introspector("sqlite", args.database)
-    with open(args.modelname, "w") as sys.stdout:
-        print_models(intro)
+    model = create_model(args)
+    save_model(args, model)
