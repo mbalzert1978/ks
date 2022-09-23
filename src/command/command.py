697a3eb4baee4bae06ee748f463799da1200e7ce
Markus Iorio <markus.iorio@centralnic.com>
2022-09-23 12:22:56 +0000

    err from helperlib

--- src/command/command.py
+++ src/command/command.py
@@ -6,14 +6,9 @@ from typing import Any, Optional
 class Command(ABC):
     def __init__(self) -> None:
         super().__init__()
-        self.err_msg: Optional[str] = None
 
     def _set_app(self, app: Any) -> None:
         self._app = app
-
-    def err(self) -> None:
-        sys.stderr.write("\033[91m%s\033[0m\n" % self.err_msg)
-        sys.stderr.flush()
 
     @abstractmethod
     def execute(self) -> Optional[Any]:
