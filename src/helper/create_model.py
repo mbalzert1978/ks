45e2c80328f7f1873e783a3cc8469db59b67ecad
Markus Iorio <markus.iorio@centralnic.com>
2022-11-10 08:02:17 +0000

    using argparse

--- src/helper/create_model.py
+++ src/helper/create_model.py
@@ -1,12 +1,14 @@
 import sys
 
+from src.parser.option_parser import get_create_model_parser
+
 from .helperlib import err, format_str, save_model_to_file
 from .model.model_creator import Creator
 from .model.sqlite_connection import Sq3LiteConnection
 
 
 def create_model(args) -> str:
-    sqlite = Sq3LiteConnection(db=args)
+    sqlite = Sq3LiteConnection(db=args.database)
     create = Creator(sqlite)
     model = create.create_model()
     model = format_str(model)
@@ -14,10 +16,7 @@ def create_model(args) -> str:
 
 
 def main() -> None:
-    raw_argv = sys.argv
-    *_, db = raw_argv
-    if len(raw_argv) < 1:
-        err('Missing required parameter "database"')
-        sys.exit(1)
-    model = create_model(db)
+    parser = get_create_model_parser()
+    args = parser.parse_args()
+    model = create_model(args)
     save_model_to_file(model)
