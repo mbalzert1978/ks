dc9aabf198aad6e6be1e97bc0470f322c33cc54b
Markus Iorio <markus.iorio@centralnic.com>
2022-11-10 08:03:04 +0000

    test

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-from src.helper.model.model_creator import *
+import sys
 
-atr = Attribute("AlbumID", "str", null=Null(True))
+from src.helper.helperlib import Validator, err
+from src.parser.option_parser import get_main_parser
 
 
-print(atr)
+if __name__ == "__main__":
+    raw_argv = sys.argv
+    if not Validator.is_model():
+        err("Missing required file 'model.py'.")
+        print("Use create_model.py to create a model.")
+        sys.exit(1)
+
+    parser = get_main_parser()
+    args = parser.parse_args()
+    print()
