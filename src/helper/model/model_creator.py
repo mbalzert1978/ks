9bfda2d8dd845dd29741beb415ac7b4d8f010355
Markus Iorio <markus.iorio@centralnic.com>
2022-11-11 07:40:00 +0000

    refactor

--- src/helper/model/model_creator.py
+++ src/helper/model/model_creator.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from .base import Connection
+from .base import Repository
 from .container import (
     Attribute,
     ClassRepresentation,
@@ -11,8 +11,8 @@ from .container import (
 
 
 class Creator:
-    def __init__(self, connection: Connection) -> None:
-        self.connection = connection
+    def __init__(self, repo: Repository) -> None:
+        self.repo = repo
 
     def create_model(self) -> str:
         head = self._get_header()
@@ -41,41 +41,32 @@ class Creator:
     def _get_tables(self) -> list[tuple[str, ...]]:
         query = (
             "SELECT name FROM sqlite_schema WHERE "
-            "type ='table' AND name NOT LIKE 'sqlite_%';"
+            "type ='table' AND name NOT LIKE 'sql%';"
         )
-        return self.connection.fetch_all(query)
-
-    def _get_primary_keys(self, table: str) -> list[tuple[str, ...]]:
-        query = (
-            "SELECT l.name FROM pragma_table_info('%s') "
-            "as l WHERE l.pk <> 0;" % table
-        )
-        return self.connection.fetch_all(query)
+        return self.repo.fetch_all(query)
 
     def _get_foreign_keys(self, table: str) -> list[tuple[str, ...]]:
-        query = "SELECT * FROM pragma_foreign_key_list('%s');" % table
-        return self.connection.fetch_all(query)
+        query = "pragma foreign_key_list('%s');" % table
+        return self.repo.fetch_all(query)
 
     def _generate_attributes(self, table: str) -> list[Attribute]:
         query = "pragma table_info('%s');" % table
-        result = self.connection.fetch_all(query=query)
-        primary_cache = {
-            name: PrimaryKey(name=name)
-            for name, *_ in self._get_primary_keys(table)
-        }
-        foreign_cache = {
+        result = self.repo.fetch_all(query=query)
+        foreign_keys = {
             name: ForeignKey(name=name, from_table=from_table, to=to)
             for _, _, from_table, name, to, *_ in self._get_foreign_keys(table)
         }
         attributes = []
-        for _, name, type_, not_null, *_ in result:
+        for _, name, type_, not_null, _, pk in result:
             not_null = Null(null=bool(not_null))
             attributes.append(
                 Attribute(
                     name=name,
                     type=self._get_type(type_),
-                    primary=primary_cache.get(name, PrimaryKey()),
-                    foreign=foreign_cache.get(name, ForeignKey()),
+                    primary=PrimaryKey(name=name)
+                    if bool(pk)
+                    else PrimaryKey(),
+                    foreign=foreign_keys.get(name, ForeignKey()),
                     null=not_null,
                 )
             )
