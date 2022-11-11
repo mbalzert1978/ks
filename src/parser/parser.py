21707d733da4249d8161aea6c85e2be668e12032
Markus Iorio <markus.iorio@centralnic.com>
2022-11-11 14:34:19 +0000

    todo better arg parsing

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -10,23 +10,34 @@ def get_main_parser() -> argparse.ArgumentParser:
         help="URI of the database you want to access."
         "Example: 'sqlite:///database.db'",
     )
+    ap(dest="table", help="Name of the table you want to access.", type=str)
     ap(
-        dest="table",
-        help="Name of the table you want to access.",
+        dest="filename",
+        help="The filename to use for the output file. Default = 'tablename.csv'",
+        default="",
     )
     ap(
         "-d",
         "--delimiter",
         dest="delimiter",
-        help="The delimiter to use for the output.csv file. Default = ';'",
+        help="The delimiter to use for the output file. Default = ';'",
         default=";",
     )
-    ap(
-        "-f",
-        "--filename",
-        dest="filename",
-        help="The filename to use for the output.csv file.",
-        default=False,
+    mut = parser.add_mutually_exclusive_group()
+    gp = mut.add_argument
+    gp(
+        "-j",
+        "--json",
+        dest="json",
+        help="Generate a JSON representation of the table.",
+        action="store_true",
+    )
+    gp(
+        "-c",
+        "--csv",
+        dest="csv",
+        help="Generate a CSV representation of the table.",
+        action="store_true",
     )
     return parser
 
